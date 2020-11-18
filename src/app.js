56b0fe20a1b9e9ead4b556126d832eaf58701e2a
Aman <patilaman080@gmail.com>
2020-11-18 17:28:30 +0530

    3-created endpoints for api requsts adn tested in postman all are working

--- src/app.js
+++ src/app.js
@@ -5,11 +5,26 @@ const MensRanking=require('../src/models/mens')
 require('../src/db/conn')
 const Port=process.env.PORT || 3000
 
-app.get('/',async (req,res)=>{
-    res.send('hello for patil')
 
+
+app.post('/api/users',(req,res)=>{
+    res.send('create')
 })
 
+app.get('/api/users',(req,res)=>{
+    res.send('read')
+})
+
+app.get('/api/users/:id',(req,res)=>{
+    res.send('byid')
+})
+
+app.patch('/api/users',(req,res)=>{
+    res.send('update')
+})
+app.delete('/api/users',(req,res)=>{
+    res.send('delete')
+})
 
 app.listen(Port,()=>{
     console.log(`server is up and running`)
