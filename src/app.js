3db7ca1da73321d7cd5d576fac34f2581b2c8eb2
Aman <patilaman080@gmail.com>
2020-11-18 17:13:56 +0530

    2-created mongodb connection and created userschema

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 console.log('running')
 const express=require('express')
 const app=express()
+const MensRanking=require('../src/models/mens')
+require('../src/db/conn')
 const Port=process.env.PORT || 3000
 
 app.get('/',async (req,res)=>{
--- src/db/conn.js
+++ src/db/conn.js
@@ -1,1 +1,13 @@
-const
+const mongoose= require('mongoose')
+
+mongoose.connect("mongodb://localhost:27017/olympics",{
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useFindAndModify:true,
+    useUnifiedTopology:true
+}).then((data)=>{
+    console.log('Connection Successfully')
+
+}).catch((err)=>{
+    console.log('No connection')
+})
